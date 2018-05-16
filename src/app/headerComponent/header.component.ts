af1f13d71bc35bec402cd0202e624f041f8c8844
ramkdas12 <ramkdas12@gmail.com>
2018-05-16 12:06:53 +0530

    Updating Header Components with functionality

--- src/app/headerComponent/header.component.ts
+++ src/app/headerComponent/header.component.ts
@@ -18,11 +18,31 @@ export class HeaderComponent {
     },
     {
       'title': 'Projects',
-      'link': ''
+      'link': '',
+      'subLinks': [
+        {
+          'title': 'Project 1',
+          'link': ''
+        },
+        {
+          'title': 'Project 2',
+          'link': ''
+        }
+      ]
     },
     {
       'title': 'Blog',
-      'link': ''
+      'link': '',
+      'subLinks': [
+        {
+          'title': 'Blog 1',
+          'link': ''
+        },
+        {
+          'title': 'Blog 2',
+          'link': ''
+        }
+      ]
     },
     {
       'title': 'Contact',
@@ -30,8 +50,54 @@ export class HeaderComponent {
     }
   ];
 
+  openNav = true;
+
   constructor() {
 
+  }
+
+  showNav = function () {
+    var classAdd = document.getElementsByClassName('navLinks')[0].className;
+    var classTest;
+    console.log(classAdd);
+    if (classAdd.indexOf('displayNav') >= 0) {
+      classTest = classAdd.replace('displayNav', ' ').trim();
+      this.openNav = true;
+    } else {
+      classTest = classAdd + ' displayNav';
+      this.openNav = false;
+    }
+    console.log(classTest);
+    document.getElementsByClassName('navLinks')[0].className = classTest;
+  };
+
+  showSub = function (event, ele) {
+    console.log(event);
+    console.log(ele)
+  };
+
+  route = function (event, links, link, sub) {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log(links);
+    for (var prevLink in links) {
+      console.log(links[prevLink]);
+      if (links[prevLink]['selected']) {
+        links[prevLink]['selected'] = false;
+      }
+      if (links[prevLink]['subLinks']) {
+        for (var prevSubLink in links[prevLink]['subLinks']) {
+          if (links[prevLink]['subLinks'][prevSubLink]['selected']) {
+            links[prevLink]['subLinks'][prevSubLink]['selected'] = false;
+          }
+        }
+      }
+    }
+    console.log(link);
+    link['selected'] = true;
+    if (sub) {
+      sub['selected'] = true;
+    }
   }
 
 }
