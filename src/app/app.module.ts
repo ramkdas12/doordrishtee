7d1005dee3db5af4290db2b949dde601e7359afb
ramkdas12 <ramkdas12@gmail.com>
2018-05-17 11:27:12 +0530

    Adding Routes to the projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,69 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 import { DataService } from './data.service';
 
 import { HeaderComponent } from './headerComponent/header.component';
+import { SectionComponent } from './sectionComponent/section.component';
+
+import { HomeComponent } from './homeComponent/home.component';
+import { AboutComponent } from './aboutComponent/about.component';
+import { ProjectComponent } from './projectComponent/project.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectComponent
+  },
+  {
+    path: 'blogs',
+    component: BlogsComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SectionComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectComponent,
+    BlogsComponent,
+    ContactComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     HttpModule
   ],
@@ -21,4 +72,5 @@ import { HeaderComponent } from './headerComponent/header.component';
   ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
